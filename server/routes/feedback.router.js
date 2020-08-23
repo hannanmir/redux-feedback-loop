6c7bb9896a8b88af1c9ec4014d9c1233f4c0e457
Hannan Mir <hannanmir2@gmail.com>
2020-08-22 23:55:49 -0500

    flag feedback, routes for flag

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -48,4 +48,22 @@ router.delete('/:id', (req, res) => {
     })
 });
 
+// PUT to flag feedback for review
+router.put('/flag/:id', (req, res) => {
+    console.log("In PUT", req.params.id);
+    let queryText = `
+        UPDATE "feedback"
+        SET "flagged" = true
+        WHERE "id" = $1;
+        `;
+    pool.query(queryText, [req.params.id])
+        .then((result) => {
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        console.log("error in PUT flag", error);
+        res.sendStatus(500);
+    });
+});
+
 module.exports = router;
--- src/components/FeedbackListItem/FeedbackListItem.jsx
+++ src/components/FeedbackListItem/FeedbackListItem.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { Button } from '@material-ui/core';
 import axios from 'axios';
 import DeleteIcon from '@material-ui/icons/Delete';
+import FlagIcon from '@material-ui/icons/Flag';
 import swal from '@sweetalert/with-react';
 
 class ArtistListItem extends Component {
@@ -29,16 +30,39 @@ class ArtistListItem extends Component {
         })
     }
 
+    flagFeedback = (id) => {
+        axios.put(`/feedback/flag/${id}`)
+        .then( () => {
+          this.props.getFeedback();
+        }).catch( (error) => {
+          console.log('error in PUT', error);
+        })
+    }
+
     render() {
-        return (
-            <tr>
-                <td>{this.props.feedback.feeling}</td>
-                <td>{this.props.feedback.understanding}</td>
-                <td>{this.props.feedback.support}</td>
-                <td>{this.props.feedback.comments}</td>
-                <td><Button variant="contained" size="small" color="secondary" startIcon={<DeleteIcon />} onClick={() => this.deleteFeedback(this.props.feedback.id)}>DELETE</Button></td>
-            </tr>
-        );
+        if (this.props.feedback.flagged === true) {
+            return (
+                <tr>
+                    <td>{this.props.feedback.feeling}</td>
+                    <td>{this.props.feedback.understanding}</td>
+                    <td>{this.props.feedback.support}</td>
+                    <td>{this.props.feedback.comments}</td>
+                    <td><Button variant="contained" size="small" color="primary" startIcon={<FlagIcon />}>Flagged for Review</Button></td>
+                    <td><Button variant="contained" size="small" color="secondary" startIcon={<DeleteIcon />} onClick={() => this.deleteFeedback(this.props.feedback.id)}>DELETE</Button></td>
+                </tr>
+            );
+        } else {
+            return (
+                <tr>
+                    <td>{this.props.feedback.feeling}</td>
+                    <td>{this.props.feedback.understanding}</td>
+                    <td>{this.props.feedback.support}</td>
+                    <td>{this.props.feedback.comments}</td>
+                    <td><Button variant="contained" size="small" startIcon={<FlagIcon />} onClick={() => this.flagFeedback(this.props.feedback.id)}>Flag for Review</Button></td>
+                    <td><Button variant="contained" size="small" color="secondary" startIcon={<DeleteIcon />} onClick={() => this.deleteFeedback(this.props.feedback.id)}>DELETE</Button></td>
+                </tr>
+            );
+        }
     }
 }
 
